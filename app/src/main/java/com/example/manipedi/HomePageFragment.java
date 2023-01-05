70e2d22e0a7bcff3a31a359449b2b67bed0e5396
Avital <gitavital@gmail.com>
2023-01-05 22:24:08 +0200

    building the menu ui

---
+++ app/src/main/java/com/example/manipedi/HomePageFragment.java
@@ -0,0 +1,34 @@
+package com.example.manipedi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.manipedi.databinding.FragmentHomePageBinding;
+
+public class HomePageFragment extends Fragment {
+    FragmentHomePageBinding binding;
+
+//    public static HomePageFragment newInstance(String param1, String param2) {
+//        HomePageFragment fragment = new HomePageFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentHomePageBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+}
--- app/src/main/java/com/example/manipedi/MainActivity.java
+++ app/src/main/java/com/example/manipedi/MainActivity.java
@@ -1,8 +1,22 @@
 package com.example.manipedi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.LayoutInflaterCompat;
+import androidx.core.view.LayoutInflaterFactory;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    int fragmentMenuId = 0;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_bar,menu);
+        if (fragmentMenuId != 0){
+            menu.removeItem(fragmentMenuId);
+        }
+        fragmentMenuId = 0;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_btn:
+                return true;
+            case R.id.search_btn:
+                return true;
+            case R.id.chat_btn:
+                return true;
+            case R.id.profile_btn:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
