c57c5b992c807ce8ae71f3619adae1a4731136a4
shirley <shirleychesner@gmail.com>
2023-01-09 09:17:52 +0200

    more styling for signin page

--- app/src/main/java/com/example/manipedi/loginFragments/LoginFragmentManager.java
+++ app/src/main/java/com/example/manipedi/loginFragments/LoginFragmentManager.java
@@ -3,6 +3,7 @@ package com.example.manipedi.loginFragments;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.manipedi.R;
 
@@ -14,10 +15,22 @@ public class LoginFragmentManager {
     }
 
     public void changeFragment(Class<? extends Fragment> fragmentClass) {
-        fragmentManager.beginTransaction()
+        this.changeFragment(fragmentClass, false);
+    }
+
+    public void changeFragment(Class<? extends Fragment> fragmentClass, boolean addToBackStack) {
+        FragmentTransaction transaction= fragmentManager.beginTransaction()
+//                .setCustomAnimations(
+//                        R.anim.slide_in_bottom,  // enter
+//                        R.anim.fade_out,  // exit
+//                        R.anim.fade_in,   // popEnter
+//                        R.anim.slide_out_buttom  // popExit
+//                )
                 .replace(R.id.loginHomePageFragment, fragmentClass, null)
-                .setReorderingAllowed(true)
-                .addToBackStack(fragmentClass.getName()) // name can be null
-                .commit();
+                .setReorderingAllowed(true);
+
+        if (addToBackStack) transaction.addToBackStack(fragmentClass.getName());
+
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/manipedi/loginFragments/SignUpFragment.java
+++ app/src/main/java/com/example/manipedi/loginFragments/SignUpFragment.java
@@ -34,7 +34,6 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         LoginFragmentManager fragmentManager = new LoginFragmentManager(getActivity());
         UserAuthentication userAuthentication = UserAuthentication.getInstance();
