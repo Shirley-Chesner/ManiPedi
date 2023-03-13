bbe5e382253f625bde79b16239fe6bccf643b090
Avital <gitavital@gmail.com>
2023-03-13 22:29:14 +0200

    oops

--- app/src/main/java/com/example/manipedi/DB/room/AppLocalDB.java
+++ app/src/main/java/com/example/manipedi/DB/room/AppLocalDB.java
@@ -1,10 +1,10 @@
-package com.example.manipedi.model;
+package com.example.manipedi.DB.room;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.example.manipedi.ManiPediApplication;
+import com.example.manipedi.DB.room.ManiPediApplication;
 
 @Database(entities = {Post.class}, version = 101, exportSchema = false)
 abstract class AppLocalDbRepository extends RoomDatabase {
